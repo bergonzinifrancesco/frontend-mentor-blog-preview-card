3e5f13e8c28d5f03887377f2bdaf563d5f4d3e8c
francescobergonzini <francesco.bergonzini@webion.it>
2024-12-28 16:31:28 +0100

    feat: improve Learning tab

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <div class="flex flex-col gap-[24px] w-[384px] h-[522px] border-2 border-black rounded-[20px] bg-white p-300 z-10">
         <img src="/illustration-article.svg" alt="Illustration article" class="w-full h-[200px] rounded-[10px]"/>
         <div role="region" class="flex gap-[12px] flex-col">
-          <p class="font-bold text-gray-950 p-100 bg-yellow rounded-[4px] w-fit h-fit text-base">Learning</p>
+          <p class="font-bold text-gray-950 px-150 py-50 bg-yellow rounded-[4px] w-fit h-fit text-base">Learning</p>
           <p class="text-gray-950 text-base">Published 21 Dec 2023</p>
           <button class="font-bold text-gray-950 text-lg hover:text-yellow place-self-start">HTML & CSS foundations</button>
           <p class="text-gray-500 text-xl">These languages are the backbone of every website, defining structure, content, and presentation.</p>
