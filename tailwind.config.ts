26620fc2597385e5fe6b23350d26aab0f5db8e3f
francescobergonzini <francesco.bergonzini@webion.it>
2024-12-28 15:18:26 +0100

    feat: setup of design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,27 @@ export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
   ],
+  theme: {
+    extend: {
+      colors: {
+        "yellow": "#F4D04E",
+        "gray-950": "#111111",
+        "gray-500": "#6B6B6B"
+      },
+      spacing: {
+        "50": "4px",
+        "100": "8px",
+        "150": "12px",
+        "300": "24px"
+      },
+      fontFamily: {
+        "serif": ["Figtree"]
+      },
+      fontSize: {
+        "2xl": ["24px", "1.5"],
+        "xl": ["16px", "1.5"],
+        "base": ["14px", "1.5"]
+      },
+    }
+  }
 } satisfies Config;
